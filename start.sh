de1a40fcb630c2ba5493d8558dad6c2a0dc01ce8
codewithpassion <dominik.fretz@gmail.com>
2014-09-05 14:28:15 -0700

    fixing port

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ DIR=${PWD#}
 
 mkdir -p $DIR/log
 
-docker run -d -p 8080:80 -v $DIR/sites-enabled:/etc/nginx/sites-enabled -v $DIR/log:/var/log/nginx codewithpassion/buildserver-nginx nginx
+docker run -d -p 80:80 -v $DIR/sites-enabled:/etc/nginx/sites-enabled -v $DIR/log:/var/log/nginx codewithpassion/buildserver-nginx nginx
